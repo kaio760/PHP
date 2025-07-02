c4d0d692fc3e6829013fae7d6e0e84a025f58309
kaio760 <kaiolimacampos517@gmail.com>
2025-07-02 11:57:22 -0300

    Add initial One Piece RPG project files
    Adicionadas imagens e arquivos de código-fonte PHP para o projeto One Piece RPG, incluindo o índice principal, seleção de personagens, ajuda e páginas sobre. A página de seleção de personagens inclui lógica para exibir Akuma no Mi e classificação com base no personagem selecionado.

---
+++ kaio/RPG/One Piece/sources/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Jogo de RPG</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link rel="icon" href="../img/iconSite.png">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-dark" style="background-color:darkblue; margin-bottom: 30px;">
+        <a class="navbar-brand" href="#">
+            <img src="../img/logoChapeu.png" width="50px" height="50px" class="d-inline-block align-top" alt="">
+            <span class="navbar-item has-text-white">One Piece RPG</span>
+        </a>
+    </nav>
+
+    <div style="display:flex; justify-content: center; align-items: center; height: 100vh;">
+        <div class="box" style="width: 500px; text-align: center;">
+            <h1 class="title">Seja Bem vindo ao RPG de One Piece</h1>
+            <p class="subtitle">Selecione as opçoes para começar</p>
+            <div class="buttons is-centered">
+                <a href="jogo.php" class="button is-primary">Iniciar Jogo</a>
+                <a href="sobre.php" class="button is-info">Sobre o jogo</a>
+                <a href="ajuda.php" class="button is-warning">Ajuda</a>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
